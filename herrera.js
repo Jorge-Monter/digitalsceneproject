5edc5bbb6067f8ac48c472f28a42b0ef8a4282c4
Hugo-Herrera <34288713+Hugo-Herrera@users.noreply.github.com>
2017-12-05 15:36:42 -0800

    Create herrera.js

---
+++ herrera.js
@@ -0,0 +1,29 @@
+// draw all moons
+function drawAllMoons() {
+  for (var i = 0; i < 10;i++){
+    moveTo(randomNumber(0,320),randomNumber(0,320));
+    drawMoon(12);
+    penUp();
+  }
+}
+//draw Moon
+function drawMoon(size) {
+  penColor("powderblue");
+  for (var i = 0; i < 3;i++){
+  dot(size);
+  turnLeft(randomNumber(1,90));
+  }
+  drawSpots();
+}
+//draw moon spots
+  function drawSpots() {
+  moveForward(5);
+  penColor("gray");
+  dot(5);
+  turnRight(120);
+  moveForward(10);
+  dot(4);
+  turnRight();
+  moveForward(9);
+  dot(3);
+}

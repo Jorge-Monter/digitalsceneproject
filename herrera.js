ebb58c058b5715dfcea56565bc2fc0e072fa3b6a
Hugo-Herrera <34288713+Hugo-Herrera@users.noreply.github.com>
2017-12-06 02:24:38 -0800

    Update herrera.js

--- herrera.js
+++ herrera.js
@@ -1,23 +1,24 @@
-drawAllMoons();
-// draw all moons
+// Hugo: draw all moons
 function drawAllMoons() {
-  for (var i = 0; i < 10;i++){
+  for (var i = 0; i < 5;i++){
     moveTo(randomNumber(0,320),randomNumber(0,320));
-    drawMoon(12);
+    drawMoon();
     penUp();
   }
 }
-//draw Moon
-function drawMoon(size) {
-  penColor("powderblue");
+
+//Hugo: draw Moon
+function drawMoon() {
+  penColor(rgb(190,190,200));
   for (var i = 0; i < 3;i++){
-  dot(size);
+  dot(randomNumber(12,15));
   turnLeft(randomNumber(1,90));
   }
-  drawSpots();
+  drawCraters();
 }
-//draw moon spots
-  function drawSpots() {
+
+//Hugo: draw moon craters
+  function drawCraters() {
   moveForward(5);
   penColor("gray");
   dot(5);
@@ -27,4 +28,46 @@ function drawMoon(size) {
   turnRight();
   moveForward(9);
   dot(3);
+   }
+// Hugo: draw nebula clouds
+function drawNebula() {
+  penUp();
+  for(var i = 0; i < 100;i++) {
+  drawPurpleCloud();
+  drawBlueCloud();
+  //drawGreenCloud();
+  drawGoldCloud();
+  }
+}
+
+  //Hugo: draw purple cloud
+  function drawPurpleCloud() {
+  penColor(rgb(237,9,218,0.2));
+  moveTo(randomNumber(0,319),randomNumber(0,420));
+  dot(10);
+  }
+  //Hugo: draw blue cloud
+  function drawBlueCloud() {
+  penColor(rgb(0,75,190,0.3));
+  turnRight(120);
+  moveForward(10);
+  dot(10);
+  }
+ //Hugo: draw gold cloud
+ function drawGoldCloud() {
+  penColor(rgb(100,140,0,0.2));
+  turnRight();
+  moveForward(10);
+  dot(10);
+  }
+
+//Hugo: This is optional if you want to draw a big earth like in the image.
+//It is not yet finished
+function drawEarth() {
+  penColor("green");
+  penWidth(160);
+  turnTo(360);
+  moveTo(240,450);
+  penDown();
+  arcRight(75,100);
 }
